6836f938b64c53b4644a83e6a0fdd84770cfefa7
Awal Hossain <awalhossainofficial@gmail.com>
2026-07-21 22:12:56 +0600

    feat: add OpenAI embeddings implementation

---
+++ langchain/embeddings/1_openai_embedding.py
@@ -0,0 +1,14 @@
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+embeddings = OpenAIEmbeddings(
+     model="text-embedding-3-large",
+     dimensions=32,
+)
+
+result = embeddings.embed_query("Hello world, Dhaka is the capital of Bangladesh.")
+
+print(result)
